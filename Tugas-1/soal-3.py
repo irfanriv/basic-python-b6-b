be87c80756d8970bffe11cad0d49148c80d3bded
irfanriv <m.irfan.rivianto@gmail.com>
2021-04-23 06:10:37 +0700

    tugas minggu 1

--- Tugas-1/soal-3.py
+++ Tugas-1/soal-3.py
@@ -5,7 +5,7 @@ if x >= 70 and y >= 70 :
     print("Selamat, anda lulus!")
 elif x < 70 and y >= 70 :
     print("Anda harus mengulang ujian teori.")
-elif x >= 70 and y <= 70 :
+elif x >= 70 and y < 70 :
     print("Anda harus mengulang ujian praktek.")
 else :
     print("Anda harus mengulang ujian teori dan praktek.")
